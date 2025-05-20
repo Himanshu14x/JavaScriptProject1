4d7b6f7002d940816878f4ce1f4f9158658d6236
Himanshu <himanshu1401x@gmail.com>
2025-05-20 22:24:33 +0530

    New commands added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "Himanshu1401x@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId =2
+accountEmail = "Power@"
+accountPassword="1212"
+accountCity="Beng"
+
+console.log("accountID")
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use Strict" //This syntax will use the code as new
+
+console.log(3+3)
+console.log("Hitesh")
+let name="Himanshu"
+let age = 27
+let isLoggedIn = true
+let num = "123abc"
+let valueInNumber = Number(num)
+console.log(valueInNumber)
+let repo = 50
+console.log(`hello my name is ${name} and your name is? ${repo}`)
+const game = new String("Yoda")
+console.log(game[0])
+const aNumber = 10000
+console.log(aNumber.toLocaleString('en-IN'))
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max-min+1)+10))
+let myDate = new Date()
+console.log(myDate.toString())
